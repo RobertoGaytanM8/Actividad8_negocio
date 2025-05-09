0d71209c6633892d6923da2a3d7e328f5fc116bf
ROBERTO_ALEXIS_GAYTAN_MENDOZA <a.22308051281202@cbtis128.edu.mx>
2025-05-09 03:01:31 +0000

    enviar

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,175 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() =>
+    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class HomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _HomePageState createState() => _HomePageState();
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              height: 400,
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    height: 400,
+                    width: width + 20,
+                    child: FadeInUp(
+                        duration: Duration(seconds: 1),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image:
+                                      AssetImage('assets/images/Refugio.png'),
+                                  fit: BoxFit.fill)),
+                        )),
+                  ),
+                  Positioned(
+                    top: 70,
+                    height: 200,
+                    width: width,
+                    child: FadeInUp(
+                        duration: Duration(milliseconds: 1000),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      'https://png.pngtree.com/png-vector/20231113/ourmid/pngtree-puppy-dog-lying-innocent-png-image_10428946.png'),
+                                  fit: BoxFit.fill)),
+                        )),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  FadeInUp(
+                      duration: Duration(milliseconds: 1500),
+                      child: Text(
+                        "Bienvenido",
+                        style: TextStyle(
+                            color: Color(0xff00bda7),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30),
+                      )),
+                  FadeInUp(
+                      duration: Duration(milliseconds: 1500),
+                      child: Text(
+                        "Inicia sesion",
+                        style: TextStyle(
+                            color: Color(0xff00bda7),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30),
+                      )),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  FadeInUp(
+                      duration: Duration(milliseconds: 1700),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                            border: Border.all(color: Color(0xcd00f3ff)),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color(0x8987aac6),
+                                blurRadius: 20,
+                                offset: Offset(0, 10),
+                              )
+                            ]),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  border: Border(
+                                      bottom: BorderSide(
+                                          color: Color(0xff00afce)))),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Nombre de usuario",
+                                    hintStyle:
+                                        TextStyle(color: Colors.grey.shade700)),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              child: TextField(
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Contraseña",
+                                    hintStyle:
+                                        TextStyle(color: Colors.grey.shade700)),
+                              ),
+                            )
+                          ],
+                        ),
+                      )),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  FadeInUp(
+                      duration: Duration(milliseconds: 1700),
+                      child: Center(
+                          child: TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                "¿Olvidaste la contraseña?",
+                                style: TextStyle(color: Color(0xff004577)),
+                              )))),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  FadeInUp(
+                      duration: Duration(milliseconds: 1900),
+                      child: MaterialButton(
+                        onPressed: () {},
+                        color: Color(0xff00424e),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        height: 50,
+                        child: Center(
+                          child: Text(
+                            "Inicia Sesion",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      )),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  FadeInUp(
+                      duration: Duration(milliseconds: 2000),
+                      child: Center(
+                          child: TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                "Crea una cuenta",
+                                style: TextStyle(color: Color(0x9a003e34)),
+                              )))),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
